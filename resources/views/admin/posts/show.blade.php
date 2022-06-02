8ad979d27fb0399ff3db36b9106d622ecb70af60
riccardoferro <riccardo.ferron8@gmail.com>
2022-06-02 16:30:27 +0200

    Created show view of a post

---
+++ resources/views/admin/posts/show.blade.php
@@ -0,0 +1,56 @@
+@extends('layouts.dashboard')
+
+
+@section('content')
+
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-12">
+                <div class="d-flex justify-content-between align-items-center">
+                    <h1>Visualizzazione post </h1>
+                    {{-- Here we go to the view admin.posts.create to create a post--}}
+                    <a href="{{ route('admin.posts.index') }}" class="btn btn-primary">
+                        Tutti i posts
+                    </a>
+                </div>
+
+                <dl>
+                    <dt>Titolo</dt>
+                    <dd>{{ $post->title}}</dd>
+                    <dt>Slug</dt>
+                    <dd>{{ $post->slug}}</dd>
+                    <dt>Contenuto</dt>
+                    <dd>{{ $post->content}}</dd>
+                </dl>
+
+
+                {{-- here we can edit the post --}}
+                <a href="{{ route( 'admin.posts.edit', ['post' => $post->id] ) }}" 
+                  class="btn btn-warning">
+
+                    Modifica
+
+                </a>
+                
+                {{-- thats a form with a button to delete the post --}}
+                <form action="{{ route('admin.posts.destroy', ['post' => $post->id] ) }}" 
+                  class="d-inline-block" 
+                  method="POST">
+
+                    {{-- control --}}
+                    @csrf
+                    @method('DELETE')
+                    
+                    <button type="submit" class="btn btn-danger">
+
+                      Elimina
+
+                    </button>
+                </form>
+
+
+            </div>
+        </div>
+    </div>
+
+@endsection
