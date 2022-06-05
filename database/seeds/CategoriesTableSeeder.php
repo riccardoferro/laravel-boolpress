a4a36f834ad1193005716958dd6d6e929f3115ca
riccardoferro <riccardo.ferron8@gmail.com>
2022-06-05 22:49:45 +0200

    created seeder that insert values

---
+++ database/seeds/CategoriesTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Category;
+use Illuminate\Database\Seeder;
+
+use Illuminate\Support\Str;
+
+class CategoriesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $categories = ['Spa','Trattamenti','Cibo','Wellness'];
+
+
+        foreach ($categories as $category) {
+            $new_category_object = new Category();
+            $new_category_object->name = $category;
+            $new_category_object->slug = Str::slug($category);
+            $new_category_object->save();
+        }
+
+    }
+}
