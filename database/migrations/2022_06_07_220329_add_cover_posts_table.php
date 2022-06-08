2cb7053d3d9df87b8b76da81328dda3291da27e1
riccardoferro <riccardo.ferron8@gmail.com>
2022-06-08 15:47:31 +0200

    created migration that add column for the image post

---
+++ database/migrations/2022_06_07_220329_add_cover_posts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddCoverPostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //add column for the image post
+        Schema::table('posts', function(Blueprint $table){
+
+            $table->string('cover')->nullable()->after('slug');
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('posts', function($table){
+
+            $table->dropColumn('cover');
+
+        });
+    }
+}
