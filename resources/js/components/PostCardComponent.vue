1ddafba73a339da434bb7a2cafc6e1269e3751ee
riccardoferro <riccardo.ferron8@gmail.com>
2022-06-09 23:44:51 +0200

    create component that show details of the ours posts

---
+++ resources/js/components/PostCardComponent.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+            <img :src=" 'storage/' + cover " :alt="title" />
+
+            <div>{{ title }} </div>
+
+            <div> {{ content }} </div>
+
+    </div>
+</template>
+
+<script>
+
+    export default {
+
+       name:'PostCardComponent',
+
+       props: ['title','cover','content'],
+
+       computed: {
+            trimmedContent(){
+                    const shortContent = this.content.length > 20 ? this.content.substring(0,20) : this.content
+
+                    return shortContent;
+            }
+       }
+     
+    }
+
+</script>
