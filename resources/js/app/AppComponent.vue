6707c089ccd5f0d01a842f00d4460fad8b5ebdae
riccardoferro <riccardo.ferron8@gmail.com>
2022-06-09 23:41:51 +0200

    created app component, the core of our web application

--- resources/js/app/AppComponent.vue
+++ resources/js/app/AppComponent.vue
@@ -1,14 +1,19 @@
 <template>
-  <div>
-    work in progress
-  </div>
+    <div>
+        <HeaderComponent/>
+        <router-view></router-view>
+    </div>
 </template>
 
 <script>
 
-export default class AppComponent {
+import HeaderComponent from '../components/HeaderComponent.vue'
 
+export default {
+  name:'AppComponent',
+  components: {HeaderComponent}
 }
+
 </script>
 
 <style scoped>
