7e3ecf143c94afc7e60f8bf94b113150d121de8c
riccardoferro <riccardo.ferron8@gmail.com>
2022-06-09 23:44:29 +0200

    create component for take all the posts in our db

---
+++ resources/js/components/PostCardListComponent.vue
@@ -0,0 +1,22 @@
+<template>
+        <div>
+                <div v-for=" (post,index) in posts" :key="index">
+                        <PostCardComponent :title="post.title" :cover="post.cover" :content="post.content" />
+                </div>
+        </div>
+</template>
+
+<script>
+
+import PostCardComponent from './PostCardComponent.vue'
+
+        export default {
+            name:'PostCardListComponent',
+            
+            props: ['posts'],
+
+            components: {PostCardComponent},
+
+        }
+        
+</script>
