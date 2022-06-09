8b4a2f41ec8c0b01ccf7b18cc8bcaaf7097c2bbf
riccardoferro <riccardo.ferron8@gmail.com>
2022-06-09 23:44:09 +0200

    created component for a wrong route

---
+++ resources/js/pages/NotFoundComponent.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 text-center">
+                NOT FOUND
+            </div>
+            <div class="col-12 text-center">
+                <router-link :to=" { name: 'home' } " > back home</router-link>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'NotFoundComponent'
+}
+</script>
+
+<style>
+
+</style>
