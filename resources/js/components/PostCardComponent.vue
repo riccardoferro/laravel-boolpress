0cb3a16a0648a61fd79e738175424dcc16277f11
riccardoferro <riccardo.ferron8@gmail.com>
2022-06-12 18:54:10 +0200

    insert if
    insert router link

--- resources/js/components/PostCardComponent.vue
+++ resources/js/components/PostCardComponent.vue
@@ -1,10 +1,13 @@
 <template>
     <div>
-            <img :src=" 'storage/' + cover " :alt="title" />
+            <!-- the v-if check if the post img is null or not -->
+            <img :v-if="cover" :src=" 'storage/' + cover " :alt="title" />
 
             <div>{{ title }} </div>
 
             <div> {{ content }} </div>
+            <!-- we gonna call the route singleBLog and we pass the ID of the post that we want the detail -->
+            <router-link :to=" { name:'single-blog' , params: { id } } "> Visualizza </router-link>
 
     </div>
 </template>
@@ -15,7 +18,7 @@
 
        name:'PostCardComponent',
 
-       props: ['title','cover','content'],
+       props: ['title','cover','content','id'],
 
        computed: {
             trimmedContent(){
